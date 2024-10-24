3cc865d2e1727890f9540811a9de5b0097aca676
Ayon Ghosh <103620492+Ayon04@users.noreply.github.com>
2024-10-24 15:21:43 +0600

    Update dbConn.php

--- dbConn.php
+++ dbConn.php
@@ -1,25 +1,40 @@
 <?php
-// dbConn.php
 
-$host = 'localhost'; 
-$db = 'user'; 
-$user = 'root'; 
-$pass = ''; 
-$charset = 'utf8mb4';
+class DbConn {
+    private $host = 'localhost';
+    private $db = 'user';
+    private $user = 'root';
+    private $pass = '';
+    private $charset = 'utf8mb4';
+    protected $pdo;
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+    public function __construct() {
+        $this->connect();
+    }
 
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-     echo "Connected successfully"; 
-} catch (\PDOException $e) {
-    echo "Connection failed: " . $e->getMessage(); 
+    private function connect() {
+        $dsn = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        try {
+            $this->pdo = new PDO($dsn, $this->user, $this->pass, $options);
+             echo "Connected successfully"; 
+        } catch (\PDOException $e) {
+            
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public function getConnection() {
+        return $this->pdo;
+    }
+
+
+    
 }
+
 ?>
-
-
